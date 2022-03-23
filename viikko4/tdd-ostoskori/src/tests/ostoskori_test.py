09b4524ecb88d16de736834cf5275cbef64bc6b4
Minna Ronka <minna.ronka@gmail.com>
2022-03-23 10:46:42 +0200

    TDD added two diff products -> cart sum = sum of the products added

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,3 +26,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(mehu)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_sama_kuin_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        mehu = Tuote("Mehu", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(mehu)
+        self.assertEqual(self.kori.hinta(), 5)
+
