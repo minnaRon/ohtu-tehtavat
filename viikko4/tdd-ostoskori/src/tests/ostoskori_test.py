f0692a9d0ff7c784dbb290f1ade56720e1d40205
Minna Ronka <minna.ronka@gmail.com>
2022-03-23 10:31:13 +0200

    TDD two item in the cart when added two different products

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(),3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        mehu = Tuote("Mehu", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(mehu)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
