ded50ae70ce44e6fa2b71785832a9d5104e3e26e
Minna Ronka <minna.ronka@gmail.com>
2022-04-24 16:22:37 +0300

    Add exercise 2-3

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,44 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    '''matcher = And(
+        HasAtLeast(5, "goals"),
+        HasAtLeast(5, "assists"),
+        PlaysIn("PHI")
+    )
+    matcher = And(
+        Not(HasAtLeast(1, "goals")),
+        PlaysIn("NYR")
+    )
+    
+    matcher = And(
+        HasFewerThan(1, "goals"),
+        PlaysIn("NYR")
+    )
+    matcher = Or(
+        HasAtLeast(30, "goals"),
+        HasAtLeast(50, "assists")
+    )
+        '''
+
+    matcher = And(
+        HasAtLeast(40, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("NYI"),
+            PlaysIn("BOS")
+        )
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
